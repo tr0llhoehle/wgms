97fbb6202acb57fdd69ecd7b988e672acda4f3df
Kai Hermann <kai.uwe.hermann@gmail.com>
2012-06-16 21:24:53 +0200

    fixed the ugly bug that prevented that the style is applied to the list view
    Signed-off-by: Kai Hermann <kai.uwe.hermann@gmail.com>

--- webapp/js/wgms.js
+++ webapp/js/wgms.js
@@ -92,27 +92,49 @@ function populateListView(listview) {
 		var listItem = document.createElement('li');
         listItem.setAttribute('id','li'+i);
         listItem.setAttribute('data-icon','troll-blank');
+        listItem.setAttribute('data-corners',"false");
+        listItem.setAttribute('data-shadow',"false");
+        listItem.setAttribute('data-iconshadow',"true");
+        listItem.setAttribute('data-wrapperels',"div");
+        listItem.setAttribute('data-iconpos',"right");
+        listItem.setAttribute('data-theme',"c");
         listItem.innerHTML = "<a>"+list.entries[i].name+"</a>";
         
         listview.appendChild(listItem);
-        $('#listview').listview();
-        $('#listview').listview('refresh');
+        //$('listview').listview();
+        //$('listview').listview('refresh');
 	}
+	//$("#shopping").trigger("create");
 }
 
 $(window).load(function(){
 	list.addEntry("HATERS");
 	list.addEntry("GONNA");
 	list.addEntry("HATE");
-	var parent = document.getElementById('shoppingcontent');
-
+	//edit
+	var editparent = document.getElementById('editcontent');
+    var editlistview = document.createElement('ul');
+    editlistview.setAttribute('id','editlistview');
+    editlistview.setAttribute('data-role','listview');
+    editparent.appendChild(editlistview);
+    populateListView(editlistview);
+    
+    //shopping
+    var parent = document.getElementById('shoppingcontent');
     var listview = document.createElement('ul');
     listview.setAttribute('id','listview');
     listview.setAttribute('data-role','listview');
-                        
     parent.appendChild(listview);
-    
     populateListView(listview);
+    
+    $('listview').listview();
+    $('listview').listview('refresh');
+	$('listview').trigger("create");
+	$("#shopping").page();
+	$("#edit").page();
+	$("#shopping").trigger("create");
+	$("#edit").trigger("create");
+
 
   $('#listview').on('click', 'li', function() {
         $(this).parent().addClass("ui-icon-check"); 
