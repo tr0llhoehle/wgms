6faa86a666b4932aab389ac2ebd7ad6e97f8800e
Benjamin <kontakt@benjamin-halbrock.de>
2012-06-17 01:03:35 +0200

    synchronized

--- tomcat-project/src/de/tr0llhoehle/wgms/DbmsConnector.java
+++ tomcat-project/src/de/tr0llhoehle/wgms/DbmsConnector.java
@@ -37,7 +37,7 @@ public class DbmsConnector {
 		return instance;
 	}
 
-	private void openConnection() {
+	private synchronized void openConnection() {
 		try {
 			Class.forName(dbClassName);
 		} catch (ClassNotFoundException e1) {
@@ -57,7 +57,7 @@ public class DbmsConnector {
 		}
 	}
 
-	public void closeConnection() {
+	public synchronized void closeConnection() {
 		if (c == null) {
 
 			return;
@@ -72,7 +72,7 @@ public class DbmsConnector {
 		c = null;
 	}
 
-	public String getEmail(String username) {
+	public synchronized String getEmail(String username) {
 		if (username == null || username.trim().equals("") || !connected) {
 			return null;
 		}
@@ -97,7 +97,7 @@ public class DbmsConnector {
 		return null;
 	}
 
-	public String getPassword(String username) {
+	public synchronized String getPassword(String username) {
 		if (username == null || username.trim().equals("") || !connected) {
 			return null;
 		}
@@ -122,7 +122,7 @@ public class DbmsConnector {
 		return null;
 	}
 
-	public void checkItem(String itemId) {
+	public synchronized void checkItem(String itemId) {
 		if (itemId == null || itemId.trim().equals("") || !connected) {
 			return;
 		}
@@ -142,7 +142,7 @@ public class DbmsConnector {
 
 	}
 
-	public void uncheckItem(String itemId) {
+	public synchronized void uncheckItem(String itemId) {
 		if (itemId == null || itemId.trim().equals("") || !connected) {
 			return;
 		}
@@ -162,7 +162,7 @@ public class DbmsConnector {
 
 	}
 
-	public void insertItem(String description, String shoppingListId,
+	public synchronized void insertItem(String description, String shoppingListId,
 			String userName) {
 		if (shoppingListId == null || shoppingListId.trim().equals("")
 				|| description == null || description.trim().equals("")
@@ -185,7 +185,7 @@ public class DbmsConnector {
 
 	}
 
-	public ArrayList<TempShoppingList> getUserLists(String userName) {
+	public synchronized ArrayList<TempShoppingList> getUserLists(String userName) {
 
 		if (userName == null || userName.trim().equals("") || !connected) {
 			return null;
@@ -216,7 +216,7 @@ public class DbmsConnector {
 		return null;
 	}
 
-	public ArrayList<TempShoppingListItem> getListsItems(String listId) {
+	public synchronized ArrayList<TempShoppingListItem> getListsItems(String listId) {
 
 		if (listId == null || listId.trim().equals("") || !connected) {
 			return null;
