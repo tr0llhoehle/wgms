40e6996ac78e01af87f6f9de7ebd36c7a3ab59ce
Moritz Winter <moritz.winter@gmail.com>
2012-06-16 20:48:24 +0200

    new tomcat testfiles

---
+++ tomcat-project/src/de/tr0llhoehle/wgms/ShoppingListServlet.java
@@ -0,0 +1,56 @@
+package de.tr0llhoehle.wgms;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class ShoppingListServlet
+ */
+@WebServlet("/ShoppingListServlet")
+public class ShoppingListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShoppingListServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(true);
+		PrintWriter out = response.getWriter();
+		if (session.isNew()) {
+			String username = request.getParameter("name");
+			String password = request.getParameter("password");
+			if (this.verifyUser(username, password)) {
+				
+			}
+		} else {
+			out.write("no new Session");
+		}
+	}
+
+	protected boolean verifyUser(String username, String password) {
+		return true; //TODO stub
+	}
+
+}
