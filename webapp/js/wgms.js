458e2ba2ea2c6d43b66cdb806ec981b0794a64ad
Cakemix <mholoch@gmail.com>
2012-06-16 21:27:15 +0200

    Created a lot of methods and variables in the list class to enable the class awesome automatic syncing with the server. All ListEntriys are now stored in six different arrays defining in which state they are. (closer description in the comments of the src-code). Also did a ton of renaming so everything is consistent. (hopefully)

--- webapp/js/wgms.js
+++ webapp/js/wgms.js
@@ -1,3 +1,6 @@
+var diffPollInterval = 5000;
+var retryInterval = 5000;
+
 //An entry in the list contains the following attributes:
 //  name - the name of the item
 //  id - an unique identifier (always unique in relation to the list) to generate 
@@ -10,81 +13,131 @@ function ListEntry(id, name) {
 
 //definition of an anonymous class which acts as a list fo ListEntries.
 var list = {
-  //contains all ListEntries the webapp knows.
-  entries: [],
+  //contains all unchecked ListEntries which are synced with the server. 
+  uncheckedEntries: [],
+  //contains all unchecked ListEntries which don't have their new state
+  //synced with the server.
+  uUncheckedEntries: [],
+  //contains all checked ListEntries which are synced with the server. 
+  checkedEntries: [],
+  //contains all checked ListEntries which don't have their new state
+  //synced with the server.
+  uCheckedEntries: [],
   //contains all newly created ListEntries which weren't successfully 
   //transmitted to the server.
-  untransmittedCreatedEntries: [],
+  addedEntries: [],
   //contains all deleted ListEntries which weren't successfully transmitted
   //to the server.
-  untransmittedDeletedEntries: [],
-  //TODO: this may cause overflows when the app runs a long time, I suppose...
+  deletedEntries: [],
+  //TODO: this may cause underflows when the app runs a long time, I suppose...
   nextId: -1,
 
-  creationTimer: 0,
-
-  deletionTimer: 0,
+  //timers for the different actions we do with the server:
+  diffPollTimer: setInterval(function(){list.diffPoll()}, diffPollInterval),
+  //if one of the following variables is 0 it means no problem has occurred
+  //while transmitting information to the server.
+  deleteEntriesTimer: 0,
+  addEntriesTimer: 0,
+  checkEntriesTimer: 0,
+  uncheckEntriesTimer: 0,
+  doneShoppingTimer: 0,
 
   addEntry: function(name) {
-    this.entries.push(new ListEntry(this.nextId, name));
+    this.addedEntries.push(new ListEntry(this.nextId, name));
     this.nextId -= 1;
-    if(this.creationTimer == 0) {
-      this.creationTimer = setInterval(function(){list.transmitCreatedEntries(this.untransmittedCreatedEntries)}, 5000);
+    if(this.addEntriesTimer == 0) {
+      this.transmitAddedEntries();
     }
   },
 
-  removeEntry: function(id) {
-    for(var i = 0; i < this.entries.length; ++i) {
-      if(id == this.entries[i].id) {
-        this.entries.splice(i, 1);
-        if(this.deletionTimer == 0) {
-          this.deletionTimer = setInterval(function(){list.transmitDeletedEntries(this.untransmittedDeletedEntries)}, 5000);
+  removeEntry: function(id, inUncheckedEntries) {
+    var arr;
+    if(inUncheckedEntries) {
+      arr = uncheckedEntries;
+    } else {
+      arr = checkedEntries;
+    }
+    for(var i = 0; i < arr.length; ++i) {
+      if(id == arr[i].id) {
+        deletedEntries.push(arr[i]);
+        arr.splice(i, 1);
+        if(this.deleteEntriesTimer == 0) {
+          this.transmitDeletedEntries();
         }
         break;
       }
     }
   },
 
-  //Used to transmit creation entries. 
-  transmitCreatedEntries: function(entries) {
+  diffPoll: function() {
+    //TODO: le magic
+  },
+  
+  transmitDeletedEntries: function(entries) {
     var successful;
+    var tempDeletedEntries = this.deletedEntries;
+    //TODO: Do the actual transmitting <-- ...
     successful = confirm("Simulate transmission of created entries to the server.\nSuccess?");
-    //TODO: Do the actual transmitting...
+    //... -->
     if(successful) {
-      clearTimeout(this.creationTimer);
-      //looks quadratic but should normally be linear since entries and this.untr[...]Entries are identical most of the time.
-      for(var entry in entries) {
-        for(var i = 0; i < this.untransmittedCreatedEntries.length; ++i) {
-          if(entry.id == this.untransmittedCreatedEntries[i].id) {
-            this.untransmittedCreatedEntries.splice(i, 1);
-            break;
-          }
-        }
+      clearTimeout(deletedEntriesTimer);
+      deleteEntriesTimer = 0;
+      this.deletedEntries.splice(0, tempDeletedEntries.length);
+    } else {
+      deleteEntriesTimer = setTimeout(function(){list.transmitDeletedEntries()}, retryInterval);
+    }
+  },
+  
+  transmitAddedEntries: function() {
+    var successful;
+    var tempAddedEntries = this.addedEntries;
+    //TODO: Do the actual transmitting <-- ...
+    successful = confirm("Simulate transmission of created entries to the server.\nSuccess?");
+    //... -->
+    if(successful) {
+      clearTimeout(addEntriesTimer);
+      addEntriesTimer = 0;
+      for(var i = 0; i < tempAddedEntries.length; ++i) {
+        //TODO: DON'T use the id from tempAddedEntries but from the server's response!
+        this.uncheckedEntries.push(new ListEntry(tempAddedEntries[i].id, tempAddedEntries[i].name));
       }
-      this.creationTimer = 0;
+      this.addedEntries.splice(0, tempAddedEntries.length);
+    } else {
+      addEntriesTimer = setTimeout(function(){list.transmitAddedEntries()}, retryInterval);
     }
   },
 
-  //Used to transmit the deletion of entries.
-  transmitDeletedEntries: function(entries) {
+  transmitCheckedEntries: function() {
     var successful;
-    var temp = "Simulate transmission of deleted entries to the server.\n";
-    successful = confirm("Simulate transmission of deleted entries to the server.\nSuccess?");
-    //TODO: Do the actual transmitting...
+    var tempCheckedEntries = this.uCheckedEntries;
+    //TODO: Do the actual transmitting <-- ...
+    successful = confirm("Simulate transmission of newly checked entries to the server.\nSuccess?");
+    //... -->
     if(successful) {
-      clearTimeout(this.deletionTimer);
-      //looks quadratic but should normally be linear since entries and this.untr[...]Entries are identical most of the time.
-      for(var entry in entries) {
-        for(var i = 0; i < this.untransmittedDeletedEntries.length; ++i) {
-          if(entry.id == this.untransmittedDeletedEntries[i].id) {
-            this.untransmittedDeletedEntries.splice(i, 1);
-            break;
-          }
-        }
-      }
-      this.deletionTimer = 0;
+      clearTimeout(checkEntriesTimer);
+      checkEntriesTimer = 0;
+      this.checkedEntries.concat(tempCheckedEntries);
+      this.uCheckedEntries.splice(0, tempCheckedEntries.length);
+    } else {
+      checkEntriesTimer = setTimeout(function(){transmitCheckedEntries()}, retryInterval);
     }
-  }
+  },
+
+  transmitUncheckedEntries: function() {
+    var successful;
+    var tempUncheckedEntries = this.uUncheckedEntries;
+    //TODO: Do the actual transmitting <-- ...
+    successful = confirm("Simulate transmission of newly unchecked entries to the server.\nSuccess?");
+    //... -->
+    if(successful) {
+      clearTimeout(uncheckEntriesTimer);
+      uncheckEntriesTimer = 0;
+      this.uncheckedEntries.concat(tempUncheckedEntries);
+      this.uUncheckedEntries.splice(0, tempUncheckedEntries.length);
+    } else {
+      uncheckEntriesTimer = setTimeout(function(){transmitUncheckedEntries()}, retryInterval);
+    }
+  },
 }
 
 $(window).load(function(){
