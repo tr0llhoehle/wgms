1ba317abe1e28b382b48b93d7e0cf3a9a35f26fb
Cakemix <mholoch@gmail.com>
2012-06-16 15:21:55 +0200

    Added javascript-classes to act as a list of entries and as an entry. (so it's a basic datastructure)

--- webapp/js/wgms.js
+++ webapp/js/wgms.js
@@ -1,3 +1,31 @@
+//definition of an anonymous class which acts as a list fo ListEntries.
+//An entry in the list contains the following attributes:
+//  name - the name of the item
+//  id - an unique identifier (unique in relation to the list) to generate valid html-ids.
+//  selected - boolean value whether the entry is selected or not.
+var list = {
+  listEntries: [],
+  nextId: 0,
+
+  addEntry: function(name) {
+    var entry;
+    entry.name = name;
+    entry.id = this.nextId;
+    this.nextId += 1;
+    entry.selected = false;
+    return this.listEntries.push(entry);
+  }
+
+  removeEntry: function(id) {
+    for(var i = 0; i < listEntries.length; ++i) {
+      if(id == entry.id) {
+        listEntries.splice(i, 1);
+        break;
+      }
+    }
+  }
+}
+
 function changeIcon(e) { 
 	var change = "#"+e.id;
 	var changeUI = change+" .ui-icon";
